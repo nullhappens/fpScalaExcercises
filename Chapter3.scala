0d74985d32fa22439360ad09fc1fc2f08ca7acb2
Diego Pineda <dpineda@gawker.com>
2014-11-18 18:39:48 -0500

    added solutions to 3.3 and 3.4

--- Chapter3.scala
+++ Chapter3.scala
@@ -1,5 +1,6 @@
 package fpinscala.datastructures
 
+//sealed trait means that all implementations of this trait (type/class/scalazfunzstuffzz) are included in this file.
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A] (head:A, tail: List[A]) extends List[A]
@@ -12,7 +13,7 @@ object List {
 
 	def product(ds: List[Double]): Double = ds match {
 		case Nil => 1.0
-		case Cons(0.0, _) => 0.0
+		case Cons(0.0, t) => 0.0
 		case Cons(x, xs) => x * product(xs)
 	}
 
@@ -24,5 +25,31 @@ object List {
 	def tail[A](x: List[A]) : List[A] = x match {
 		case Nil => Nil
 		case Cons(h, t) => t
+	}
+
+	//excercise 3.3
+	def setHead[A](l: List[A], x:A): List[A] = l match {
+		case Nil => Cons(x, Nil)
+		case Cons(h, t) => Cons(x, t)
+	}
+
+	//excercise 3.4
+	def drop[A](l: List[A], n: Int): List[A] = l match {
+		case Nil => Nil
+		case Cons(h, t) =>
+			if (n <= 0)
+				Cons(h, t)
+			else
+				drop(t, n-1)
+	}
+
+	//excercise 3.5
+	def dropWhile[A] (l:List[A], f:A => Boolean): List[A] = l match {
+		case Nil => Nil
+		case Cons(h, t) =>
+			if (f(h))
+				dropWhile(t,f)
+			else
+				t
 	}
 }
